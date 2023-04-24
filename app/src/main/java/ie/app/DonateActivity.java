da4a094da54531a220dffd361a28619a12ddec3c
huetrantt21020018 <21020018@vnu.edu.vn>
2023-04-24 22:53:19 +0700

    lession02

--- app/src/main/java/ie/app/DonateActivity.java
+++ app/src/main/java/ie/app/DonateActivity.java
@@ -1,13 +1,21 @@
 package ie.app;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 public class DonateActivity extends AppCompatActivity {
+
+    private Button donateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,24 @@ public class DonateActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action",
+                                Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+*/
+
+        donateButton = (Button) findViewById(R.id.donateButton);
+        if (donateButton != null)
+        {
+            Log.v("Donate", "Really got the donate button");
+        }
+
     }
 
     @Override
