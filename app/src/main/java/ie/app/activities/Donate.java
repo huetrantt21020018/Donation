249ba4ad23c19d43a01017ebfdba20129c6e4985
huetrantt21020018 <21020018@vnu.edu.vn>
2023-04-26 11:07:36 +0700

    lession 08

--- app/src/main/java/ie/app/activities/Donate.java
+++ app/src/main/java/ie/app/activities/Donate.java
@@ -1,10 +1,7 @@
 package ie.app.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.Button;
@@ -13,9 +10,7 @@ import android.widget.RadioGroup;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import ie.app.R;
@@ -68,22 +63,6 @@ public class Donate extends Base {
         amountPicker.setMaxValue(1000);
         progressBar.setMax(10000);
         amountTotal.setText("$0");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_donate, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            case R.id.menuReport : startActivity (new Intent(this, Report.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     // handle button pressed
--- app/src/main/java/ie/app/activities/Report.java
+++ app/src/main/java/ie/app/activities/Report.java
@@ -1,15 +1,6 @@
 package ie.app.activities;
 
-import static ie.app.activities.Base.donations;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import ie.app.R;
@@ -30,22 +21,4 @@ public class Report extends Base
         DonationAdapter adapter = new DonationAdapter(this, donations);
         listView.setAdapter(adapter);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_donate, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.menuDonate : startActivity (new Intent(this, Donate.class));
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
