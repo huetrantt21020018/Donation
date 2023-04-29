d93a354f4fbc6cc1096d0c1a108de69315e2046a
huetrantt21020018 <21020018@vnu.edu.vn>
2023-04-29 14:53:03 +0700

    lession 12 - request error

--- app/src/main/java/ie/app/activities/Report.java
+++ app/src/main/java/ie/app/activities/Report.java
@@ -1,15 +1,60 @@
 package ie.app.activities;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import java.util.List;
 
 import ie.app.R;
 import ie.app.adapters.DonationAdapter;
+import ie.app.api.DonationApi;
+import ie.app.models.Donation;
 
 
 public class Report extends Base
 {
     ListView listView;
+
+    private class GetAllTask extends AsyncTask<String, Void, List<Donation>> {
+        protected ProgressDialog dialog;
+        protected Context context;
+        public GetAllTask(Context context)
+        {
+            this.context = context;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.dialog = new ProgressDialog(context, 1);
+            this.dialog.setMessage("Retrieving Donations List");
+            this.dialog.show();
+        }
+        @Override
+        protected List<Donation> doInBackground(String... params) {
+            try {
+                Log.v("donate", "Donation App Getting All Donations");
+                return (List<Donation>) DonationApi.getAll((String) params[0]);
+            }
+            catch (Exception e) {
+                Log.v("donate", "ERROR : " + e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(List<Donation> result) {
+            super.onPostExecute(result);
+            //use result to calculate the totalDonated amount here
+            //progressBar.setProgress(app.totalDonated);
+            //amountTotal.setText("$" + app.totalDonated);
+            if (dialog.isShowing())
+                dialog.dismiss();
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -20,5 +65,11 @@ public class Report extends Base
         listView = (ListView) findViewById(R.id.reportList);
         DonationAdapter adapter = new DonationAdapter(this, app.donations);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        new GetAllTask(this).execute("/donations");
     }
 }
