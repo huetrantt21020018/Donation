a8ef8221ffadfcae5d0d8800874ca0b24b61be99
huetrantt21020018 <21020018@vnu.edu.vn>
2023-04-29 12:13:34 +0700

    initial commit

--- app/src/main/java/ie/app/activities/Base.java
+++ app/src/main/java/ie/app/activities/Base.java
@@ -15,14 +15,14 @@ public class Base extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         app = (DonationApp) getApplication();
-        app.dbManager.open();
-        app.dbManager.setTotalDonated(this);
+        //app.dbManager.open();
+        //app.dbManager.setTotalDonated(this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        app.dbManager.close();
+        //app.dbManager.close();
     }
 
     @Override
@@ -38,7 +38,7 @@ public class Base extends AppCompatActivity {
         MenuItem report = menu.findItem(R.id.menuReport);
         MenuItem donate = menu.findItem(R.id.menuDonate);
         MenuItem reset = menu.findItem(R.id.menuReset);
-        if(app.dbManager.getAll().isEmpty())
+        if(app.donations.isEmpty())
         {
             report.setEnabled(false);
             reset.setEnabled(false);
@@ -49,7 +49,7 @@ public class Base extends AppCompatActivity {
         }
         if(this instanceof Donate){
             donate.setVisible(false);
-            if(!app.dbManager.getAll().isEmpty())
+            if(!app.donations.isEmpty())
             {
                 report.setVisible(true);
                 reset.setEnabled(true);
--- app/src/main/java/ie/app/activities/Donate.java
+++ app/src/main/java/ie/app/activities/Donate.java
@@ -91,7 +91,8 @@ public class Donate extends Base {
     @Override
     public void reset(MenuItem item)
     {
-        app.dbManager.reset();
+        //app.dbManager.reset();
+        app.donations.clear();
         app.totalDonated = 0;
         amountTotal.setText("$" + app.totalDonated);
         progressBar.setProgress(app.totalDonated);
